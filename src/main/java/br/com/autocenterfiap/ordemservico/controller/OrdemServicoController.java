9a551b0740ed8a54cd123fcaaaf922d6a5713f86
William Fernandes <wiilfern1910@outlook.com>
2026-04-29 20:00:33 -0300

    fix: Endpoint trocado para Patch

--- src/main/java/br/com/autocenterfiap/ordemservico/controller/OrdemServicoController.java
+++ src/main/java/br/com/autocenterfiap/ordemservico/controller/OrdemServicoController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,7 +58,7 @@ public class OrdemServicoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<OrdemServicoResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid OrdemServicoUpdateDTO dto) {
         return ResponseEntity.ok(ordemServicoService.atualizar(id, dto));
     }

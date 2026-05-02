4ae0e47cf0cab00fbc8b6c3ae4ccfd1f6e2716f7
William Fernandes <wiilfern1910@outlook.com>
2026-05-02 14:26:09 -0300

    fix: Ajuste de Migrations

--- db/migration/V11__insere_itens_os.sql
+++ db/migration/V11__insere_itens_os.sql
@@ -8,8 +8,8 @@ INSERT INTO os_item_servico (
     data_hora_inicio, data_hora_fim, data_criacao, data_ultima_atualizacao
 ) VALUES (
      3, 1, 150.00, 'FINALIZADO',
-     DATEADD('HOUR', -4, CURRENT_TIMESTAMP),
-     DATEADD('MINUTE', -170, CURRENT_TIMESTAMP), -- 2h50 atrás
+     TIMESTAMPADD(HOUR, -4, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(MINUTE, -170, CURRENT_TIMESTAMP), -- 2h50 atrás
      CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
  );
 
@@ -19,8 +19,8 @@ INSERT INTO os_item_servico (
     data_hora_inicio, data_hora_fim, data_criacao, data_ultima_atualizacao
 ) VALUES (
      3, 2, 120.00, 'FINALIZADO',
-     DATEADD('HOUR', -3, CURRENT_TIMESTAMP),
-     DATEADD('MINUTE', -140, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(HOUR, -3, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(MINUTE, -140, CURRENT_TIMESTAMP),
      CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
  );
 
@@ -30,8 +30,8 @@ INSERT INTO os_item_servico (
     data_hora_inicio, data_hora_fim, data_criacao, data_ultima_atualizacao
 ) VALUES (
      3, 3, 350.00, 'FINALIZADO',
-     DATEADD('HOUR', -6, CURRENT_TIMESTAMP),
-     DATEADD('HOUR', -4, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(HOUR, -6, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(HOUR, -4, CURRENT_TIMESTAMP),
      CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
  );
 
@@ -45,8 +45,8 @@ INSERT INTO os_item_servico (
     data_hora_inicio, data_hora_fim, data_criacao, data_ultima_atualizacao
 ) VALUES (
      4, 1, 150.00, 'FINALIZADO',
-     DATEADD('HOUR', -2, CURRENT_TIMESTAMP),
-     DATEADD('MINUTE', -70, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(HOUR, -2, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(MINUTE, -70, CURRENT_TIMESTAMP),
      CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
  );
 
@@ -56,8 +56,8 @@ INSERT INTO os_item_servico (
     data_hora_inicio, data_hora_fim, data_criacao, data_ultima_atualizacao
 ) VALUES (
      4, 2, 120.00, 'FINALIZADO',
-     DATEADD('HOUR', -2, CURRENT_TIMESTAMP),
-     DATEADD('MINUTE', -55, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(HOUR, -2, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(MINUTE, -55, CURRENT_TIMESTAMP),
      CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
  );
 
@@ -67,7 +67,7 @@ INSERT INTO os_item_servico (
     data_hora_inicio, data_hora_fim, data_criacao, data_ultima_atualizacao
 ) VALUES (
      4, 3, 350.00, 'FINALIZADO',
-     DATEADD('HOUR', -5, CURRENT_TIMESTAMP),
-     DATEADD('MINUTE', -150, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(HOUR, -5, CURRENT_TIMESTAMP),
+     TIMESTAMPADD(MINUTE, -150, CURRENT_TIMESTAMP),
      CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
  );

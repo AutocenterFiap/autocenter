4623ee89804b8fbebc8e1de6081811fe3e063397
William Fernandes <wiilfern1910@outlook.com>
2026-05-02 14:50:54 -0300

    feat: Altera Status da OS na aprovacao/reprovacao do Orcamento

--- src/main/java/br/com/autocenterfiap/orcamento/service/OrcamentoService.java
+++ src/main/java/br/com/autocenterfiap/orcamento/service/OrcamentoService.java
@@ -4,15 +4,13 @@ import br.com.autocenterfiap.orcamento.exception.OrcamentoNaoEncontradoException
 import br.com.autocenterfiap.orcamento.repository.OrcamentoRepository;
 import br.com.autocenterfiap.orcamento.repository.entity.Orcamento;
 import br.com.autocenterfiap.ordemservico.enums.StatusOS;
-import br.com.autocenterfiap.ordemservico.model.OSItemServico;
 import br.com.autocenterfiap.ordemservico.repository.OrdemServicoRepository;
 import br.com.autocenterfiap.ordemservico.model.OrdemServico;
 import br.com.autocenterfiap.util.Util;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import static br.com.autocenterfiap.orcamento.enums.StatusOrcamento.AGUARDANDO_APROVACAO;
@@ -23,20 +21,25 @@ public class OrcamentoService {
     private final OrcamentoRepository orcamentoRepository;
     private final OrdemServicoRepository ordemServicoRepository;
 
+    @Transactional
     public Orcamento aprovar(Long orcamentoId) {
         Orcamento orcamento = orcamentoRepository.findById(orcamentoId)
                 .orElseThrow(() -> new OrcamentoNaoEncontradoException(orcamentoId));
         orcamento.aprovar();
+        orcamento.getOrdemServico().setStatusOS(StatusOS.APROVADA);
         return orcamentoRepository.save(orcamento);
     }
 
+    @Transactional
     public Orcamento reprovar(Long orcamentoId) {
         Orcamento orcamento = orcamentoRepository.findById(orcamentoId)
                 .orElseThrow(() -> new OrcamentoNaoEncontradoException(orcamentoId));
         orcamento.reprovar();
+        orcamento.getOrdemServico().setStatusOS(StatusOS.CANCELADA);
         return orcamentoRepository.save(orcamento);
     }
 
+    @Transactional
     public void gerarOrcamento() {
         List<OrdemServico> ordens = ordemServicoRepository.findByStatus(StatusOS.AGUARDANDO_APROVACAO);
 

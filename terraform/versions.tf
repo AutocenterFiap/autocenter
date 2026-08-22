0c97340309b4330ad8bc6d287c7668ede5dc5f96
Felipe Neaime <gitlab_admin_c8ff20@example.com>
2026-08-21 21:39:46 -0300

    config: Configurando deploy aws com terraform

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,21 +1,19 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.15.0"
 
   required_providers {
-    # Provider para gerenciar recursos Kubernetes
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.27"
-    }
-    # Provider para instalar charts Helm (Metrics Server para o HPA)
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.12"
-    }
-    # Provider para executar comandos locais (criar cluster Kind, build Docker)
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.0"
+      version = "3.2.0"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }

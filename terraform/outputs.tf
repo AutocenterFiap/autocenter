5dff7069b507ca8a52a2ee4b3cb37db45ecf9d8f
Felipe Neaime <gitlab_admin_c8ff20@example.com>
2026-09-13 19:53:01 -0300

    Ajusta output para ser consultado pelo repositorio do gateway.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "app_service_hostname" {
   description = "Hostname do LoadBalancer criado para a aplicacao"
   value       = try(kubernetes_service.autocenter_app.status[0].load_balancer[0].ingress[0].hostname, null)
 }
+
+output "application_service_name" {
+  description = "Nome do Service Kubernetes do app"
+  value       = kubernetes_service.autocenter_app.metadata[0].name
+}
